5c756a18284817cd27e7a15a46e7e49e88bb7ca3
dougto <douglas22toledo@gmail.com>
2020-05-23 19:12:21 -0300

    added auth middleware

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import UserController from './controllers/UserController';
 import ChatController from './controllers/ChatController';
 
+import authMiddleware from './middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/users/:id', UserController.getById);
-routes.get('/users', UserController.getUsersByTags);
+routes.get('/users/:id', authMiddleware, UserController.getById);
+routes.get('/users', authMiddleware, UserController.getUsersByTags);
 routes.post('/users', UserController.createUser);
-routes.put('/users/:id/tags', UserController.updateTags);
+routes.put('/users/:id/tags', authMiddleware, UserController.updateTags);
 
-routes.get('/chat/', ChatController.getChat);
-routes.post('/chat', ChatController.postMessage);
+routes.get('/chat/', authMiddleware, ChatController.getChat);
+routes.post('/chat', authMiddleware, ChatController.postMessage);
 
 export default routes;
