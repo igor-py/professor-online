6e1e1e1ed0e363bf73a39c72e09068f952a6dca3
Igor Marins <igormarins6@gmail.com>
2020-05-23 15:27:02 -0300

    Pronto pra dar uma merge

--- app/src/pages/inscreva-se/index.js
+++ app/src/pages/inscreva-se/index.js
@@ -3,11 +3,53 @@ import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
 import './style.css';
 
-export default function InscrevaSe() {
+// Somente chamar categorias quando for cadastro de professor
+const Categorias = () => {
 
     return (
 
+        <fieldset className="inputs">
+            <legend>Categoria</legend>
+            <select>
+                <option>Música</option>
+                <option>WEB</option>
+                <option>Graduação</option>
+                <option>Finanças</option>
+                <option>Mobile</option>
+                <option>Banco de Dados</option>
+                <option>Fotografia</option>
+                <option>Gerenciamento</option>
+            </select>
+        </fieldset>
+    )
+};
 
+const Inscrever = () => {
+
+    return (
+
+        <fieldset className="inputs">
+            <label for="login">Nome</label>
+            <input id="login" className="entradas" type="text" placeholder="Coloque aqui o seu nome e sobrenome"
+                required></input>
+
+            <label for="senha">Senha</label>
+            <input id="senha" className="entradas" type="password" required placeholder="Coloque aqui a sua senha"></input>
+
+            <label for="senha-repete">Repita a senha</label>
+            <input id="senha-repete" className="entradas" type="password" required placeholder="Repita a senha"></input>
+
+            <label for="email">E-mail</label>
+            <input id="email" className="entradas" type="email" placeholder="Coloque aqui o seu G-mail"
+                required></input>
+        </fieldset>
+    )
+}
+
+
+export default function InscrevaSe() {
+
+    return (
 
         <div id="inscreva-se">
             <Header />
@@ -15,24 +57,10 @@ export default function InscrevaSe() {
             <main>
                 <h2>Inscreva-Se</h2>
 
-                <div id="inputs">
-                    <label for="login">Nome</label>
-                    <input id="login" className="entradas" type="text" placeholder="Coloque aqui o seu nome e sobrenome"
-                    required></input>
+                <div className="inputs">
+                    <Inscrever />
 
-                    <label for="senha">Senha</label>
-                    <input id="senha" className="entradas" type="password" required placeholder="Coloque aqui a sua senha"></input>
-
-                    <label for="senha-repete">Repita a senha</label>
-                    <input id="senha-repete" className="entradas" type="password" required placeholder="Repita a senha"></input>
-
-                    <label for="email">E-mail</label>
-                    <input id="email" className="entradas" type="email" placeholder="Coloque aqui o seu G-mail" 
-                    required></input>
-
-                    <label className="checkbox">
-                    <input type="checkbox"></input> Marque apenas se for professor
-                    </label>
+                    <Categorias className="entradas" />
                 </div>
 
             </main>
