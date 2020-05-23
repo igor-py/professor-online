15d102507a3e42cb5f3ab0d59732aabed3655dab
dougto <douglas22toledo@gmail.com>
2020-05-23 20:31:47 -0300

    adjusted page height

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import InscrevaSe from './pages/cadastrar';
 import Home from './pages/home';
 import Pesquisa from './pages/pesquisa';
@@ -7,16 +8,22 @@ import Entrar from './pages/entrar';
 import Header from '../src/Components/Header';
 import Footer from '../src/Components/Footer';
 
+const PageContainer = styled.div`
+  min-height: 80vh;
+`;
+
 export default function Routes() {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/entrar" component={Entrar} />
-        <Route path="/cadastrar" component={InscrevaSe} />
-        <Route path="/pesquisa" component={Pesquisa} />
-      </Switch>
+      <PageContainer>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/entrar" component={Entrar} />
+          <Route path="/cadastrar" component={InscrevaSe} />
+          <Route path="/pesquisa" component={Pesquisa} />
+        </Switch>
+      </PageContainer>
       <Footer />
     </BrowserRouter>
   );
