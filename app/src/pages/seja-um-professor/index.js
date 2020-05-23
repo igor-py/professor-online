b4c5a2fb0878e62de090bc39323597083f46a45e
Mateus <mateusvalexandre@gmail.com>
2020-05-23 15:28:43 -0300

    seja um professor

--- app/src/pages/seja-um-professor/index.js
+++ app/src/pages/seja-um-professor/index.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import Header from '../../Components/Header';
 import { ProfessorItemList, ProfessorItemContainer } from './styles';
+import './styles.css';
 
 export default function SejaUmProfessorPage() {
     return (
       <ProfessorItemContainer>
       <Header />
       <ProfessorItemList>
-        <li>
-          <h1>Professor Online</h1>
-        </li>
-        <li>
-          <input type='text'/>
-        </li>
-        <li>
-          <p1>Aqui vai uma imagem</p1>
-        </li>
+        
+        <body>
+          <li>
+            <h1>Vantagens</h1>
+          </li>
+          <li>
+            <div class="paragrafos">
+              <li class="vantagem">Possibilidade de mudar vidas;</li>
+              <li class="vantagem">Possibilidade de mudar o mundo ao seu redor;</li>
+              <li class="vantagem">Aprendizado constante;</li>
+              <li class="vantagem">Casa dia é um dia diferente;</li>
+              <li class="vantagem">Possibilidade constante de se fazer novas amizades;</li>
+              <div class="frase-motivacional">
+                <li class="frase">Feliz aquele que transfere o que sabe e aprende o que ensina.</li>
+                <li class="autor">Autor: Cora Coralina</li>
+              </div>      
+            </div>
+            <button class="botao">Inscreva-se</button>
+          </li>
+        </body>
+        
       </ProfessorItemList>
       </ProfessorItemContainer>
     );
