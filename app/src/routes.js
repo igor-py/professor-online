402a21d43e02b45f0b7de186bedf942da43efd1e
Igor Marins <igormarins6@gmail.com>
2020-05-23 16:38:49 -0300

    Colocando links entre as paginas de cadastros

--- app/src/routes.js
+++ app/src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SejaUmProfessorPage from './pages/seja-um-professor';
 import InscrevaSe from './pages/inscreva-se'
+import Professor from './pages/inscreva-se/professor';
+import Aluno from './pages/inscreva-se/aluno';
 import Home from './Components/Home';
 
 export default function Routes() {
@@ -11,6 +13,8 @@ export default function Routes() {
         <Route path="/" exact component={Home}/>
         <Route path="/sejaumprofessor" component={SejaUmProfessorPage}/>
         <Route path="/inscreva-se" component={InscrevaSe}/>
+        <Route path="/inscreva-aluno" component={Aluno}/>
+        <Route path="/inscreva-professor" component={Professor}/>
       </Switch>
     </BrowserRouter>
   );
