8a6879e475ce0575d492a58934a7ba6ef9a2dc64
dougto <douglas22toledo@gmail.com>
2020-05-23 20:01:33 -0300

    change header

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import '../styles/Header.css';
-import professor from '../images/professor.png'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import professor from '../images/professor.png';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-export default function Header(){
-    return (
-        <div id="headerContainer">
-            <div id="logoContainer">
-                <img src= {professor} alt= "Professor Online" />
-            </div>
-            <div id="labelContainer">
-                <Link to="/entrar" className="links-header" >Entrar</Link>
-                <Link to="/sejaumprofessor" className="links-header">Seja um professor</Link>
-                <Link to="/inscreva-se" className="links-header">Inscreva-se</Link>
-            </div>
-        </div>
-    );
+export default function Header() {
+  return (
+    <div id="headerContainer">
+      <div id="logoContainer">
+        <img src={professor} alt="Professor Online" />
+      </div>
+      <div id="labelContainer">
+        <Link to="/cadastrar" className="links-header">
+          Cadastrar
+        </Link>
+        <Link to="/entrar" className="links-header">
+          Entrar
+        </Link>
+      </div>
+    </div>
+  );
 }
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SejaUmProfessorPage from './pages/seja-um-professor';
-import InscrevaSe from './pages/inscreva-se'
+import InscrevaSe from './pages/inscreva-se';
 import Professor from './pages/inscreva-se/professor';
 import Aluno from './pages/inscreva-se/aluno';
 import Home from './Components/Home';
-import Pesquisa from './pages/pesquisa'
+import Pesquisa from './pages/pesquisa';
 
 export default function Routes() {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/sejaumprofessor" component={SejaUmProfessorPage}/>
-        <Route path="/inscreva-se" component={InscrevaSe}/>
-        <Route path="/pesquisa" component={Pesquisa}/>
-        <Route path="/inscreva-aluno" component={Aluno}/>
-        <Route path="/inscreva-professor" component={Professor}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/sejaumprofessor" component={SejaUmProfessorPage} />
+        <Route path="/cadastrar" component={InscrevaSe} />
+        <Route path="/pesquisa" component={Pesquisa} />
+        <Route path="/inscreva-aluno" component={Aluno} />
+        <Route path="/inscreva-professor" component={Professor} />
       </Switch>
     </BrowserRouter>
   );
