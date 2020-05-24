764f7ff1a59a5ae5b65ca803c59b434e46c9cf94
dougto <douglas22toledo@gmail.com>
2020-05-24 19:12:59 -0300

    some style changes

--- app/src/pages/pesquisa/index.js
+++ app/src/pages/pesquisa/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import User from '../../Components/User';
 import '../../styles/pesquisa.css';
-import proffLogo from '../../images/professor.png'
+import proffLogo from '../../images/professor.png';
 import api from '../../services/api';
 
 export default class Pesquisa extends Component {
@@ -47,46 +47,48 @@ export default class Pesquisa extends Component {
         turn: 'tarde',
         rating: 3.3,
         aula: 'react',
-      },],
+      },
+    ],
   };
 
-  handleInputChange = e => {
-    this.setState({ 
-      newUser: e.target.value
-    })
-  }
+  handleInputChange = (e) => {
+    this.setState({
+      newUser: e.target.value,
+    });
+  };
 
-  handleSubmit = async e => {
+  handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { newUser } = this.state
+    const { newUser } = this.state;
 
     const response = await api.post({
       data: {
-        tags: newUser
-      }
-    })
+        tags: newUser,
+      },
+    });
 
     this.setState({
       users: [...response],
       newUser: '',
-    })
-  }
+    });
+  };
 
   render() {
     const { users, newUser } = this.state;
     return (
-      <div>
+      <div id="parentContainer">
         <div id="tituloContainer">
-          <img src={proffLogo}/>
+          <img src={proffLogo} />
           <form id="submitContainer" onSubmit={this.handleSubmit}>
-            <input type="text" 
+            <input
+              type="text"
               placeholder="Busque Aqui um Curso"
               value={newUser}
               onChange={this.handleInputChange}
             />
             <div id="submitButton">
-              <button type="submit"/>
+              <button type="submit" />
             </div>
           </form>
         </div>
