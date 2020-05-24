3dc5c0b503cbf7a8297eda107f9850c3b59c20c5
Igor Marins <igormarins6@gmail.com>
2020-05-23 22:09:19 -0300

    Criando pasta para pagina de perfil

---
+++ app/src/pages/perfil/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: red;
+
+`;
+
+export default function Perfil(){
+
+    
+}
+
