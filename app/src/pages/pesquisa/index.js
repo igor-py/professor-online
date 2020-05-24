060eb4c64cd251ba56503c0c47c0249b0f7d0705
leandrodeveng <leandro.lt187@gmail.com>
2020-05-24 17:50:25 -0300

    merge develop into develop

--- app/src/pages/pesquisa/index.js
+++ app/src/pages/pesquisa/index.js
@@ -2,51 +2,12 @@ import React, { Component } from 'react';
 import User from '../../Components/User';
 import '../../styles/pesquisa.css';
 import proffLogo from '../../images/professor.png'
+import api from '../../services/api';
 
 export default class Pesquisa extends Component {
   state = {
     newUser: '',
-    users: [
-      {
-        id: 1,
-        name: 'Igor Marins',
-        email: 'igor@gmail.com',
-        password: '12345',
-        isTeacher: true,
-        turn: 'manhã',
-        rating: 5,
-        aula: 'português',
-      },
-      {
-        id: 2,
-        name: 'Douglas Toledo',
-        email: 'douglas@gmail.com',
-        password: '12345',
-        isTeacher: true,
-        turn: 'tarde',
-        rating: 4.3,
-        aula: 'matemática',
-      },
-      {
-        id: 3,
-        name: 'Leandro Trindade',
-        email: 'leandro@gmail.com',
-        password: '12345',
-        isTeacher: true,
-        turn: 'tarde',
-        rating: 3.9,
-        aula: 'react',
-      },
-      {
-        id: 4,
-        name: 'Vitão da Massa',
-        email: 'vitor@gmail.com',
-        password: '12345',
-        isTeacher: true,
-        turn: 'tarde',
-        rating: 3.3,
-        aula: 'react',
-      },],
+    users: [],
   };
 
   handleInputChange = e => {
@@ -55,19 +16,25 @@ export default class Pesquisa extends Component {
     })
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    //Fazer a chamada à API aqui, buscando os dados
-    //Dentro do set state, setar o users:[] com os dados buscados
+    const { newUser } = this.state
+
+    const response = await api.post({
+      data: {
+        tags: newUser
+      }
+    })
 
     this.setState({
+      users: [...response],
       newUser: '',
     })
   }
 
   render() {
-    const { users, newUser, resultadoText } = this.state;
+    const { users, newUser } = this.state;
     return (
       <div>
         <div id="tituloContainer">
