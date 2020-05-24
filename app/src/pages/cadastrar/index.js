8bc9d2609af6528a3a368be2037cfac84bf358b8
dougto <douglas22toledo@gmail.com>
2020-05-23 21:28:39 -0300

    refactoring cadastrar

--- app/src/pages/cadastrar/index.js
+++ app/src/pages/cadastrar/index.js
@@ -1,32 +1,56 @@
-import React, { Component } from 'react';
-import '../../styles/inscreva.css';
-import mulher_1 from '../../images/mulher1.png';
-import mulher_2 from '../../images/mulher2.png';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import styled from 'styled-components';
+import professor from '../../images/professor-eis.png';
+import student from '../../images/student.png';
 
-export default function InscrevaSe() {
+const Container = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  border: 2px solid #999;
+  border-radius: 140px;
+  height: 280px;
+  width: 280px;
+`;
+
+const EntityContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+`;
+
+const Title = styled.div`
+  font: 36px Arial;
+  font-weight: bold;
+  margin-bottom: 16px;
+`;
+
+export default function Cadastrar() {
   return (
     <>
-      <div id="inscrevaContainer">
-        <div id="professorContainer">
-          <label>Professor</label>
-          <img
-            src={mulher_1}
-            alt="Mulher Experiente representando professora"
-          ></img>
-          <Link to="/inscreva-professor" className="links-inscrever">
-            Seja um Professor
-          </Link>
-        </div>
+      <Container>
+        <EntityContainer>
+          <Title>Sou um professor</Title>
+          <Image src={professor} alt="professor"></Image>
+        </EntityContainer>
 
-        <div id="alunoContainer">
-          <label>Aluno</label>
-          <img src={mulher_2} alt="Mulher representando aluna"></img>
-          <Link to="/inscreva-aluno" className="links-inscrever">
-            Seja um Aluno
-          </Link>
-        </div>
-      </div>
+        <EntityContainer>
+          <Title>Sou um aluno</Title>
+          <Image src={student} alt="student"></Image>
+        </EntityContainer>
+      </Container>
     </>
   );
 }
